2231d1f1efa7839292c34005abad6def19c20759
Your Name <you@example.com>
2022-03-25 22:15:23 -0400

    Berkan

--- TestModule2/src/Test1/deneme1.java
+++ TestModule2/src/Test1/deneme1.java
@@ -3,5 +3,15 @@ package Test1;
 public class deneme1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        /*
+        echo "# berkanDemoProject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BerkanUgurlu/berkanDemoProject.git
+git push -u origin main
+         */
     }
 }
